24e08aa763482d49c6e85b7f28b0fde6a79b569c
helmi2701 <helminurhidayat614@gmail.com>
2022-06-18 20:40:55 +0700

    upload

---
+++ gudangBahan/dashboard.php
@@ -0,0 +1,34 @@
+<div class="page-inner">
+					<div class="page-header">
+						<h4 class="page-title">Dashboard</h4>
+						<ul class="breadcrumbs">
+							<li class="nav-home">
+								<a href="#">
+									<i class="flaticon-home"></i>
+								</a>
+							</li>
+							<li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+							<li class="nav-item">
+								<a href="#">Dashboard</a>
+							</li>
+							<li class="separator">
+								<i class="flaticon-right-arrow"></i>
+							</li>
+							<li class="nav-item">
+								<a href="#">Dashboard</a>
+							</li>
+						</ul>
+					</div>
+					<div class="row">
+						<div class="col-md-12">
+							<div class="card">
+								<center>
+									<h4>SISTEM INFORMASI INVENTORY BARANG </h4>
+									<h4>BERBASIS WEB</h4></center>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
